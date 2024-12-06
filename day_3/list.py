ce08b9f1802398c5ab2466f32dbce31f3b9fa28b
troydelageto <tambongroy7@gmail.com>
2024-12-06 23:30:08 +0100

    module 5 exercise on lists

---
+++ day_3/list.py
@@ -0,0 +1,29 @@
+# Module 5: lists
+
+# ======================
+# Exercises level 1
+# ======================
+
+# Declaring an empty list
+list1=[]
+
+# Declaring a list with 5 elements
+
+list_2=[5,7,9,6,3]
+
+#The length of the list
+
+x = len(list_2)
+print(f"length of list_2 is {x}")
+
+# The first item of the list
+first= list_2[0]
+print(f"first item is {first}")
+
+# The middle item of the list
+first= list_2[2]
+print(f"middle item is {first}")
+
+# The last item of the list
+first= list_2[-1]
+print(f"last item is {first}")
