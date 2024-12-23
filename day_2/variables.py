b2299ae5793a391b7549c89975c0f05b628a0bb2
troydelageto <tambongroy7@gmail.com>
2024-12-23 12:33:21 +0100

    Module 2,3 and 4 Exercies

--- day_2/variables.py
+++ day_2/variables.py
@@ -3,7 +3,7 @@ import math
 
 #Exercises: Level 1
 
-# Declaring different python data types
+#1 Declaring different python data types
 first_name = "lio"
 last_name =  "Ahmed"
 full_name= "Salli Abakar"
@@ -18,7 +18,7 @@ x,y,ops =1,2,"multiply"
 
 #Exercises : Level 2
 
-# printing out types
+# 1 printing out types
 print(type(first_name))
 print(type(last_name))
 print(type(full_name))
@@ -33,64 +33,64 @@ print(type(x))
 print(type(y))
 print(type(ops))
 
-# printing length of first_name
+#2 printing length of first_name
 print(len(first_name))
 
-# Comparing lenghts of first_name and last_name
+#3 Comparing lenghts of first_name and last_name
 if len(first_name)==len(last_name):
     print("equal lengths")
 else:
     print("Unequal length")
     
-# declaring varaibles num_one and num_two
+#4 declaring varaibles num_one and num_two
 num_one = 5
 num_two = 4
 
-#Addition of variables
+#5 Addition of variables
 total = num_one + num_two
 print(f"total is {total}")
 
-#Subtraction of numbers
+#6 Subtraction of numbers
 diff= num_one-num_two
 print(f"diff is {diff}")
 
-#multiplicaiton of numbers
+#7 multiplicaiton of numbers
 product = num_one * num_two
 print(f"product is {product}")
 
-#Division of numbers
+#8 Division of numbers
 division = num_one/num_two
 print(f"Division is {division}")
 
-#Modolus of num
+#9 Modolus of num
 remainder = num_one % num_two
 print(f"remainder is {remainder}")
 
-#exponential of a number
+#10 exponential of a number
 exp=num_one**num_two
 print(f"the exponent is {exp}")
 
-#floor division
+#11 floor division
 
 floor_division = math.floor(num_one / num_two)
 print(f"floor Division is {floor_division}")
 
-#area of a circle
+#12-i area of a circle
 radius = 30
 pi=math.pi
 area_of_circle = pi * radius**2
 print(f"the area of circle is {area_of_circle}")
 
-#circumference of ccircle
+#12-ii circumference of circle
 circum_of_circle = 2*pi*radius
 print(f"the circumference is {circum_of_circle}")
 
-#calculating area from user input
+#12-iii calculating area from user input
 radius = int(input("enter the raius of the circle"))
 area = pi * radius**2
 print(f"the new area is {area}")
 
-#collecting and printing input data from a user
+#13 collecting and printing input data from a user
 first_name = input("Please enter first name")
 last_name = input ("please enter last name")
 country = input("please enter your country")
@@ -98,6 +98,6 @@ age = input("please write your age")
 
 print(f"your first name is {first_name}\n last name is {last_name} \n country is {country}\n age is {age}")
 
-#printing help funtion
+#14 printing help funtion
 
 print(help())
